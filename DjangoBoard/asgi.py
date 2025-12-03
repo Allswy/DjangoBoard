ea85b84eba5bf6fcfc72a555db06d04744566c74
Allswy <swyswy123@outlook.com>
2025-12-03 20:04:22 +0800

    First commit of project files

---
+++ DjangoBoard/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DjangoBoard project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoBoard.settings')
+
+application = get_asgi_application()
